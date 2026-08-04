a2044b7bb0cd59911d936e3581a8adafdad36fda
Teekam-Saini <teekamsingodiyasaini@gmail.com>
2026-08-04 10:15:09 +0000

    adding hdl bits problem solution

---
+++ hdlbits/sequential/flip-flops/15-edge-detectors.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output reg [7:0] pedge
+);
+
+    reg [7:0] in_prev;
+
+    always @(posedge clk) begin
+        
+        pedge <= in & ~in_prev;
+        in_prev <= in;
+    end
+
+endmodule
